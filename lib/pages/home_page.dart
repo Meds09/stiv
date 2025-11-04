3366af76000adfe5c5600fc51b23c26199a1d3b7
Meds <pipemesil97@gmail.com>
2025-11-03 21:16:10 -0500

    Homepage implementation 1.0

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,29 +1,116 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:stiv/shared/theme/theme_data.dart';
 
-class HomePage extends StatelessWidget {
-final user = FirebaseAuth.instance.currentUser!;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
-//sign out user
-  void signUserOut ()async {
-  await FirebaseAuth.instance.signOut();
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
-  HomePage({super.key});
+class _HomePageState extends State<HomePage> {
+  final user = FirebaseAuth.instance.currentUser!;
+
+  //sign out user
+  void signUserOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: signUserOut, icon: Icon(Icons.logout)),
-          
-        ],
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        //appbar
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/stiv-logo-blue.png',
+                        height: 50,
+                      ),
+                      Text(
+                        'Stiv',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.textPrimary,
+                        ),
+                      ),
+                    ],
+                  ),
+                  //Settings button
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: signUserOut,
+                        child: Icon(
+                          Icons.settings,
+                          color: AppColors.textPrimary,
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      //Logout button
+                      GestureDetector(
+                        onTap: signUserOut,
+                        child: Icon(Icons.logout, color: AppColors.textPrimary),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 25),
+            //welcome back message
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    'Bienvenido, ${user.displayName}',
+                    style: TextStyle(color: AppColors.textPrimary),
+                  ),
+                ),
+                //Message banner
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Text(
+                    'Comencemos realizando el diagnóstico de tus dispostivos',
+                    style: TextStyle(
+                      color: AppColors.textPrimary,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  
+                ),
+                const SizedBox(height: 24),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Divider(
+                    color: Colors.grey[350],
+                    thickness: 4,
+                    
+                  ),
+                ),
+                const SizedBox(height: 24),
+              ],
+            ),
+            const SizedBox(height: 8),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text('¡Bienvenido ${user.email}!'),
-      ),
+      //stats
+      //cards
+      //footer
     );
   }
 }
-
