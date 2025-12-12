3c058e979b50a3ecdb9782764c2593d8707b15ae
Meds <pipemesil97@gmail.com>
2025-12-11 21:39:37 -0500

    categories providers implementation

--- lib/features/diagnostic/presentation/providers/catalog_providers.dart
+++ lib/features/diagnostic/presentation/providers/catalog_providers.dart
@@ -1,1 +1,20 @@
-//TODO: IMPLEMENTAR
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stiv/features/diagnostic/data/catalog_repository_impl.dart';
+import 'package:stiv/features/diagnostic/models/catalog_repository.dart';
+import 'package:stiv/features/diagnostic/models/category.dart';
+
+//Proveedor del repositorio
+final catalogRepositoryProvider = Provider<CatalogRepository>((ref) {
+  return CatalogRepositoryImpl();
+});
+
+//lista de categorias
+final categoriesProvider = FutureProvider<List<Category>>((ref) async {
+  final repo = ref.read(catalogRepositoryProvider);
+  return repo.getCategories();
+});
+
+final deviceByCategoryProvider = FutureProvider.family((ref, int categoryId) {
+  final repo = ref.read(catalogRepositoryProvider);
+  return repo.getDevicesByCategory(categoryId);
+});
